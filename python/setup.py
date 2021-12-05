257e7fea11a55581cfae5f08baa1bfbbf54fca1f
Timothy Lin <32549553+tmthyln@users.noreply.github.com>
2021-12-05 12:36:39 -0500

    formally bump version to 0.0.2

--- python/setup.py
+++ python/setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='tabben',
-    version='0.0.2-dev',
+    version='0.0.2',
     description='A package for working with datasets from the open benchmark for tabular data',
     long_description=long_description,
     long_description_content_type='text/markdown',
