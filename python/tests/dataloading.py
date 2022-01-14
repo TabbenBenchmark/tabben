e88e0b95404959c399e153c4ad3e86d01eeabddf
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2022-01-14 13:33:12 -0500

    refactor dataset tests

---
+++ python/tests/dataloading.py
@@ -0,0 +1,57 @@
+import pytest
+from torch.utils.data import DataLoader
+
+from tabben.datasets import OpenTabularDataset
+
+
+################################################################################
+# Shared pytest fixtures (never need to be imported anywhere)
+################################################################################
+
+@pytest.fixture(scope='session')
+def data_dir(tmp_path_factory):
+    return tmp_path_factory.mktemp('data')
+
+
+################################################################################
+# Common assertions for datasets
+################################################################################
+
+def check_sizes(ds, num_examples, num_features, num_outputs=1):
+    assert len(ds) == num_examples
+    
+    # check the input and output arrays themselves
+    X, y = ds.numpy()
+    assert X.shape == (num_examples, num_features)
+    assert y.size == num_examples * num_outputs
+    
+    # test sizes for batch size of 1
+    dl = DataLoader(ds)
+    item = next(iter(dl))
+    
+    assert len(item) == 2
+    assert item[0].numel() == num_features
+    
+    # test sizes for batch size of 4
+    dl = DataLoader(ds, batch_size=4)
+    item = next(iter(dl))
+    
+    assert len(item) == 2
+    assert item[0].size() == (4, num_features)
+    
+    if num_outputs > 1:
+        assert item[1].size() == (4, num_outputs)
+    else:
+        assert item[1].numel() == 4 * num_outputs
+
+
+def check_split_sizes(directory, ds_name, splits_dict):
+    for split, expected_size in splits_dict.items():
+        ds = OpenTabularDataset(directory, ds_name, split=split)
+        check_sizes(ds, *expected_size)
+
+
+def check_attributes(directory, ds_name, attributes_dict):
+    ds = OpenTabularDataset(directory, ds_name)  # which split doesn't matter here
+    for attr_name, attr_value in attributes_dict.items():
+        assert getattr(ds, attr_name) == attr_value
--- python/tests/test_datasets.py
+++ python/tests/test_datasets.py
@@ -2,57 +2,9 @@
 Test the dataset files themselves and whether they can be loaded properly.
 """
 import pytest
+
+from python.tests.dataloading import check_attributes, check_split_sizes
 from tabben.datasets import OpenTabularDataset, validate_dataset_file
-from torch.utils.data import DataLoader
-
-
-################################################################################
-# Common Utilities
-################################################################################
-
-@pytest.fixture(scope='session')
-def data_dir(tmp_path_factory):
-    return tmp_path_factory.mktemp('data')
-
-
-def check_sizes(ds, num_examples, num_features, num_outputs=1):
-    assert len(ds) == num_examples
-    
-    # check the input and output arrays themselves
-    X, y = ds.numpy()
-    assert X.shape == (num_examples, num_features)
-    assert y.size == num_examples * num_outputs
-    
-    # test sizes for batch size of 1
-    dl = DataLoader(ds)
-    item = next(iter(dl))
-    
-    assert len(item) == 2
-    assert item[0].numel() == num_features
-    
-    # test sizes for batch size of 4
-    dl = DataLoader(ds, batch_size=4)
-    item = next(iter(dl))
-    
-    assert len(item) == 2
-    assert item[0].size() == (4, num_features)
-    
-    if num_outputs > 1:
-        assert item[1].size() == (4, num_outputs)
-    else:
-        assert item[1].numel() == 4 * num_outputs
-
-
-def check_split_sizes(directory, ds_name, splits_dict):
-    for split, expected_size in splits_dict.items():
-        ds = OpenTabularDataset(directory, ds_name, split=split)
-        check_sizes(ds, *expected_size)
-
-
-def check_attributes(directory, ds_name, attributes_dict):
-    ds = OpenTabularDataset(directory, ds_name)  # which split doesn't matter here
-    for attr_name, attr_value in attributes_dict.items():
-        assert getattr(ds, attr_name) == attr_value
 
 
 ################################################################################
