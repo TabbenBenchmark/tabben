d26805a6638a2fd3892d50ecb1f40bf72bdaf477
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2022-01-14 23:37:17 -0500

    fix failing metadata function test
    (no package code change, only test code)

--- python/tests/test_metadata_functions.py
+++ python/tests/test_metadata_functions.py
@@ -10,20 +10,20 @@ import pytest
 def test_register_dataset_failures():
     # invalid dataset names
     with pytest.raises(ValueError) as exc_info:
-        register_dataset('all')
+        register_dataset('all', data_location='')
     assert 'special name' in str(exc_info.value) and 'all' in str(exc_info.value)
     
     with pytest.raises(ValueError) as exc_info:
         with pytest.warns(UserWarning, match='lowercase'):
-            register_dataset('AlL')
+            register_dataset('AlL', data_location='')
     assert 'special name' in str(exc_info.value) and 'all' in str(exc_info.value)
     
     for dataset_name in list_datasets():
         with pytest.raises(ValueError) as exc_info:
-            register_dataset(dataset_name)
+            register_dataset(dataset_name, data_location='')
         assert 'already registered' in str(exc_info.value)
     
     # tasks
     with pytest.raises(ValueError) as exc_info:
-        register_dataset('placeholder', 'class')
+        register_dataset('placeholder', 'class', data_location='')
     assert 'unknown task' in str(exc_info.value).lower()
